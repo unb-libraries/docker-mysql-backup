87744cccd81a76ce4adc71456ce07144709f8749
Jacob Sanford <jsanford@unb.ca>
2025-11-13 19:47:41 -0400

    Remove column statistics

--- build/scripts/mysql-backup.sh
+++ build/scripts/mysql-backup.sh
@@ -83,7 +83,6 @@ backup_single_database() {
         --events \
         --triggers \
         --default-character-set=utf8mb4 \
-        --column-statistics=0 \
         --databases "$DB_NAME" \
         --tables $STRUCTURE_ONLY_TABLES_EXPANDED > "$TMP_SCHEMA"
       echo "✅ Dumped schema for structure tables to: $TMP_SCHEMA"
@@ -113,7 +112,6 @@ backup_single_database() {
     --hex-blob \
     --default-character-set=utf8mb4 \
     --set-gtid-purged=OFF \
-    --column-statistics=0 \
     $IGNORE_TABLES_FULL_DUMP_CMD \
     "$DB_NAME" > "$TMP_DATA" 2>&1; then
     echo "❌ ERROR: Failed to dump database: $DB_NAME"
